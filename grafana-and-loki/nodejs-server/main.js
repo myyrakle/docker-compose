52716d6b1be7c333bff0cda3a7c05b25acdc9528
myyrakle <sssang97@naver.com>
2025-02-10 02:53:32 +0900

    grafana-loki 예제 작성

--- grafana-and-loki/nodejs-server/main.js
+++ grafana-and-loki/nodejs-server/main.js
@@ -4,10 +4,9 @@ const serviceName = process.env.SERVICE_NAME || "NONE";
 const containerID = process.env.HOSTNAME || "NONE";
 
 const logger = winston.createLogger({
-  level: "info",
   format: winston.format.combine(
-    winston.format.timestamp(),
     winston.format((info) => {
+      info.timestamp = Date.now(); // 타임스탬프를 Unix 타임스탬프로 설정
       info.service = serviceName;
       info.containerID = containerID;
       return info;
@@ -31,6 +30,8 @@ async function sleep(ms) {
 async function main() {
   while (true) {
     logger.info({ message: "Hello, World!" });
+
+    logger.error({ message: "Bye", error: new Error("Opps") });
 
     await sleep(5000);
   }
