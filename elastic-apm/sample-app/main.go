c6b073d9ed1191a8cb82fb40d35be53cbe2bd0b9
myyrakle <sssang97@naver.com>
2025-08-06 04:22:09 +0900

    [#22] elastic apm 템플릿 구성 완료

--- elastic-apm/sample-app/main.go
+++ elastic-apm/sample-app/main.go
@@ -4,46 +4,27 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"time"
 
-	"github.com/labstack/echo/v4"
-	"go.elastic.co/apm/module/apmechov4/v2"
-	"go.elastic.co/apm/v2"
+	"go.elastic.co/apm/module/apmhttp"
 )
 
 func main() {
-	e := echo.New()
-	e.Use(apmechov4.Middleware())
-
-	e.GET("/", func(c echo.Context) error {
-		tx := apm.TransactionFromContext(c.Request().Context())
-		tx.Context.SetLabel("endpoint", "home")
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"message":   "Hello from Elastic APM monitored Go app!",
-			"timestamp": time.Now().Format(time.RFC3339),
-		})
-	})
-
-	e.GET("/health", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"status":    "healthy",
-			"timestamp": time.Now().Format(time.RFC3339),
-		})
-	})
-
-	e.GET("/error", func(c echo.Context) error {
-		err := fmt.Errorf("This is a test error for APM monitoring")
-		apm.CaptureError(c.Request().Context(), err).Send()
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"error":   "Something went wrong!",
-			"message": err.Error(),
-		})
-	})
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
+	// read enviroments
+	envs := os.Environ()
+	for _, env := range envs {
+		fmt.Println(env)
 	}
-	e.Logger.Infof("Go Echo server running on port %s", port)
-	e.Logger.Fatal(e.Start(":" + port))
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, Elastic APM!"))
+	})
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	http.ListenAndServe(":8080", apmhttp.Wrap(mux))
 }
